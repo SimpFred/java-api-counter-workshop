9c795482202fc3bd59a2c5e54f9d8b3129debd21
Simon Fredriksson <128689713+SimpFred@users.noreply.github.com>
2024-08-28 09:57:50 +0200

    added test testGetSpecificJoke()

--- src/test/java/com/booleanuk/api/JokeMachineControllerTest.java
+++ src/test/java/com/booleanuk/api/JokeMachineControllerTest.java
@@ -11,4 +11,16 @@ public class JokeMachineControllerTest {
         String joke = controller.getJoke();
         Assertions.assertTrue(controller.getJokes().contains(joke));
     }
+
+    @Test
+    public void testGetSpecificJoke() {
+        JokeMachineController controller = new JokeMachineController();
+        String joke = controller.getJokes().get(0);
+        Assertions.assertEquals("What sort of car does an egg drive? ---- A yolkswagen!", controller.getJokes().get(0));
+        Assertions.assertEquals("Why did the tomato turn red? ---- Because it saw the salad dressing!", controller.getJokes().get(1));
+        Assertions.assertEquals("What do you call a bear with no teeth? ---- A gummy bear!", controller.getJokes().get(2));
+        Assertions.assertEquals("What do you call a pile of cats? ---- A meowtain!", controller.getJokes().get(3));
+        Assertions.assertEquals("Why did the scarecrow win an award? ---- Because he was outstanding in his field!", controller.getJokes().get(4));
+        Assertions.assertEquals("What do you call a fish wearing a crown? ---- A kingfish!", controller.getJokes().get(5));
+    }
 }
